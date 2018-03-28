35f376a621f81ad363f14d3b3df885ee583e718f
b-richard <benjamin.richard-exterieur@fiducial.net>
2018-03-28 17:49:25 +0200

    No more need to overload initFromRequest in EntityConverter that represents associations. Just extends ListAbstractConverter.
    For simple entity, extends ItemAbstractConverter instead of AbstractConverter. using AbstractConverter::initFromRequest has been deprecated coz it's now available from the 2 others Abstract classes

---
+++ src/Request/ParamConverter/ItemAbstractConverter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Rebolon\Request\ParamConverter;
+
+use ApiPlatform\Core\Bridge\Symfony\Validator\Exception\ValidationException;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+/**
+ *
+ * Class ItemAbstractConverter
+ * @package Rebolon\Request\ParamConverter\Library
+ */
+abstract class ItemAbstractConverter extends AbstractConverter
+{
+    /**
+     * @inheritdoc
+     */
+    public function initFromRequest($jsonOrArray, $propertyPath)
+    {
+        try {
+            self::$propertyPath[] = $propertyPath;
+
+            $json = $this->checkJsonOrArray($jsonOrArray);
+
+            $idPropertyIsInJson = false;
+            if (!is_array($json)
+                || ($idPropertyIsInJson = array_key_exists($this->getIdProperty(), $json))
+            ) {
+                /**
+                 * We don't care of other properties. We don't accept update on sub-entity, we can create or re-use
+                 * So here we just clean json and replace it with the id content
+                 */
+                if ($idPropertyIsInJson) {
+                    $json = $json[$this->getIdProperty()];
+                }
+
+                array_pop(self::$propertyPath);
+
+                return $this->getFromDatabase($json);
+            }
+
+            $entity = $this->buildEntity($json);
+
+            array_pop(self::$propertyPath);
+
+            return $entity;
+        } catch (ValidationException $e) {
+            throw $e;
+        } catch (\Exception $e) {
+            $violationList = new ConstraintViolationList();
+            $violation = new ConstraintViolation($e->getMessage(), null, [], null, $this->getPropertyPath(), null);
+            $violationList->add($violation);
+            throw new ValidationException(
+                $violationList,
+                sprintf('Wrong parameter to create new %s (generic)', static::RELATED_ENTITY),
+                420,
+                $e
+            );
+        }
+    }
+}
---
+++ src/Request/ParamConverter/ListAbstractConverter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Rebolon\Request\ParamConverter;
+
+use ApiPlatform\Core\Bridge\Symfony\Validator\Exception\ValidationException;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+/**
+ *
+ * Class ListAbstractConverter
+ * @package Rebolon\Request\ParamConverter\Library
+ */
+abstract class ListAbstractConverter extends AbstractConverter
+{
+    /**
+     * @inheritdoc
+     */
+    public function initFromRequest($jsonOrArray, $propertyPath)
+    {
+        self::$propertyPath[] = $propertyPath;
+
+        $json = $this->checkJsonOrArray($jsonOrArray);
+
+        // the API accept authors as one object or as an array of object, so i need to transform at least in one array
+        $listItems = $json;
+        if (is_object($json)) {
+            $listItems = [$json];
+        }
+
+        $entities = [];
+        try {
+            foreach ($listItems as $item) {
+                self::$propertyPath[count(self::$propertyPath)] = '[' . count($entities) . ']';
+
+                $entities[] = $this->buildEntity($item);
+
+                array_pop(self::$propertyPath);
+            }
+        } catch (ValidationException $e) {
+            throw $e;
+        } catch (\Exception $e) {
+            $violationList = new ConstraintViolationList();
+            $violation = new ConstraintViolation($e->getMessage(), null, [], null, implode('.', self::$propertyPath), null);
+            $violationList->add($violation);
+            throw new ValidationException($violationList, sprintf('Wrong parameter to create new %s (generic)', static::RELATED_ENTITY), 420, $e);
+        } finally {
+            array_pop(self::$propertyPath);
+        }
+
+        return $entities;
+    }
+}
