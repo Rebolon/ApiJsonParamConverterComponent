14efc737f247b211e808fd8f474dd3e5cd81a6bf
b-richard <benjamin.richard-exterieur@fiducial.net>
2018-03-29 16:34:34 +0200

    Add an update feature (previously only insert was allowed) but it adds more complexity: should i allow update of all nodes when an id is set or only root element of the json string ? what happens on associative entity (ParamConverter must not remove DB entries, so the controller will have to manage this) ? should i block this feature when this is an HTTP POST call ?

--- src/Request/ParamConverter/ItemAbstractConverter.php
+++ src/Request/ParamConverter/ItemAbstractConverter.php
@@ -14,6 +14,11 @@ use Symfony\Component\Validator\ConstraintViolationList;
 abstract class ItemAbstractConverter extends AbstractConverter
 {
     /**
+     * only root element can be updated, all nested entities won't be even if they have ID. All other fields of thoses entities will be forgiven
+     */
+    const ROOT_LIMIT_FOR_UPDATE = 1;
+
+    /**
      * @inheritdoc
      */
     public function initFromRequest($jsonOrArray, $propertyPath)
@@ -24,23 +29,31 @@ abstract class ItemAbstractConverter extends AbstractConverter
             $json = $this->checkJsonOrArray($jsonOrArray);
 
             $idPropertyIsInJson = false;
+            $entity = null;
             if (!is_array($json)
                 || ($idPropertyIsInJson = array_key_exists($this->getIdProperty(), $json))
             ) {
-                /**
-                 * We don't care of other properties. We don't accept update on sub-entity, we can create or re-use
-                 * So here we just clean json and replace it with the id content
-                 */
-                if ($idPropertyIsInJson) {
-                    $json = $json[$this->getIdProperty()];
+
+                if (count(self::$propertyPath) > self::ROOT_LIMIT_FOR_UPDATE) {
+                    /**
+                     * We don't care of other properties. We don't accept update on sub-entity, we can create or re-use
+                     * So here we just clean json and replace it with the id content
+                     */
+                    if ($idPropertyIsInJson) {
+                        $json = $json[$this->getIdProperty()];
+                    }
+
+                    array_pop(self::$propertyPath);
+
+                    return $this->getFromDatabase($json);
                 }
 
-                array_pop(self::$propertyPath);
-
-                return $this->getFromDatabase($json);
+                if ($idPropertyIsInJson) {
+                    $entity = $this->getFromDatabase($json[$this->getIdProperty()], static::RELATED_ENTITY);
+                }
             }
 
-            $entity = $this->buildEntity($json);
+            $entity = $this->buildEntity($json, $entity);
 
             array_pop(self::$propertyPath);
 
